89efdd4842df312158231dbaeb99d7b19ef27faa
silvajpedro <belojpedro.31@gmail.com>
2022-09-25 20:26:04 -0300

    first commit

---
+++ src/components/Principal.js
@@ -0,0 +1,19 @@
+import React from "react";
+import * as S from "./stilo"
+export default function Principal(){
+    return( 
+        <div>
+
+    <S.Header> 
+        <nav>
+            <ul>
+                <li> About</li>
+                  <li>Recipes</li>
+                <li>Subscribe</li>
+            </ul>
+        </nav>
+    </S.Header>
+        </div>
+
+    )
+}
