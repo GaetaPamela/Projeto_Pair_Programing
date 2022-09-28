bd6a504bc4dc0736b96e538ed782805d3d2cab63
silvajpedro <belojpedro.31@gmail.com>
2022-09-27 22:33:18 -0300

    begining of main

--- src/components/stilo.js
+++ src/components/stilo.js
@@ -15,7 +15,6 @@ background-image:url(${Recipes});
 height: 145vh;
 background-size: 100%;
 background-repeat: no-repeat;
-border: solid yellow;
 h2{
     position: relative;
     top: 3.5vh;
@@ -27,7 +26,6 @@ nav{
     justify-content: flex-end;
     align-items: center;
     width: 91vw;
-    border: solid red;
     height: 15vh;
     ul{
         display: flex;
@@ -36,7 +34,6 @@ nav{
         list-style: none;
         width: 35vw;
         height: 10vh;
-        border: solid green;
         li{
             display: flex;
             align-items: center;
@@ -61,4 +58,83 @@ h1{
     color: #373737;
     font-weight: 500;
 }
+`
+export const Main = styled.main`
+position: relative;
+top: 4vh;
+height: 200vh;
+background-color: #F2F4F1;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+div{
+    position: relative;
+    top: -43vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 20vh;
+    h3{
+        font-size:25px;
+        color: #373737;
+        letter-spacing: 1px;
+        transition: ease-in-out 0.4s;
+        &:hover,&:hover + span{
+            transform: scale(115%);
+        }
+    }
+    span{
+        position: relative;
+        top: 2.5vh;
+        background-color: #000;
+        width: 5.5vw;
+        height: 0.5vh;
+        transition: ease-in-out 0.4s;
+    }
+}
+section{
+    position: relative;
+    top: -38vh;
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+    height: 100vh;
+    border: solid;
+}
+`
+export const FigureBox = styled.figure`
+display: flex;
+flex-direction: column;
+align-items: center;
+overflow: hidden;
+width: 29.2vw;
+height: 90vh;
+background-color: #ffff;
+box-shadow:  0px  4px  5px   grey;
+span{
+    position: relative;
+    top: 20vh;
+    background-color:#000;
+    width: 4vw;
+    height: 0.8vh;
+}
+figcaption{
+    font-family: "Questrial", sans-serif;
+    position: relative;
+    top: 25vh;
+    font-size: 28px;
+    cursor: pointer;
+    &:hover{
+        transform: scale(105%);
+    }
+}
+`
+export const Images = styled.img`
+height: 45vh;
+transition: ease-in-out 0.5s;
+&:hover{
+transform: scale(110%);
+};
 `
